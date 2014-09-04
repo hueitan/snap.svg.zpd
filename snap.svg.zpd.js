149ba4bea6177077cb25820d2159b73e1b61ff59
tikiatua <tikiatua@gmail.com>
2014-09-04 14:21:35 +0200

    move functions to zpd scope

--- snap.svg.zpd.js
+++ snap.svg.zpd.js
@@ -307,33 +307,6 @@
                 return p;
             }
 
-            /**
-             * Sets the current transform matrix of an element.
-             */
-            var setCTM = function (element, matrix) {
-                var s = "matrix(" + matrix.a + "," + matrix.b + "," + matrix.c + "," + matrix.d + "," + matrix.e + "," + matrix.f + ")";
-
-                element.setAttribute("transform", s);
-            };
-
-            /**
-             * Dumps a matrix to a string (useful for debug).
-             */
-            function dumpMatrix(matrix) {
-                var s = "[ " + matrix.a + ", " + matrix.c + ", " + matrix.e + "\n  " + matrix.b + ", " + matrix.d + ", " + matrix.f + "\n  0, 0, 1 ]";
-
-                return s;
-            }
-
-            /**
-             * Sets attributes of an element.
-             */
-            function setAttributes(element, attributes) {
-                for (var i in attributes) {
-                    element.setAttributeNS(null, i, attributes[i]);
-                }
-
-            }
 
             /**
              * Handle mouse wheel event.
